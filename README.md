23d007ca3cbeca4c52ebfd7d8991b2e49c47e27f
ruchi-kansara <69650172+ruchi-kansara@users.noreply.github.com>
2023-01-25 03:21:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-basic-login
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-bjchww)
---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component, VERSION } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    if (this.f.username.value == 'admin' && this.f.password.value == 'admin') {
+      alert('Success');
+    } else {
+      alert('failure');
+    }
+  }
+}
